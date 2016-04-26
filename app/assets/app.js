296d59e64bdb6b828acf7fc216449caf188578de
ellahyvarinen <ella.hyvarinen@hotmail.com>
2016-04-26 09:53:37 +0300

    first commit

---
+++ app/assets/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+$(function() {
+
+var mymap = L.map('mapid').setView([60.101, 24.561], 10);
+//var mymap = L.map('mapid').setView([51.505, -0.09], 10);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'ellahyvarinen.pp8fe87e',
+    accessToken: 'pk.eyJ1IjoiZWxsYWh5dmFyaW5lbiIsImEiOiJjaW5md3I3bWcwMDg3dzRseGdidDR6dWNxIn0.BEptiAhxiXNJZAexSRNurQ'
+}).addTo(mymap);
+
+$('.location').on('click', function(){
+  mymap.locate({setView: true, maxZoom: 15});
+});
+
+mymap.on('locationfound', onLocationFound);
+function onLocationFound(e) {
+    console.log(e);  
+    L.marker(e.latlng).addTo(mymap).bindPopup("<b>You are here</b>").openPopup();
+		console.log(e.latlng);
+}
+
+var marker = L.marker([60.1, 24.5]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>#NewsByTwitter").openPopup();
+
+var popup = L.popup()
+	.setLatLng([60.1, 24.5]);
+  //.setContent("#NewsByTwitter")
+  //(.openOn(mymap);
+	
+});
